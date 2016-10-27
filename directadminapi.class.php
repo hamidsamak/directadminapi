7c6b35f1e70c2bf7c2bcd54adfd58be94e40d570
Hamid <hamidreza.samak@gmail.com>
2016-10-27 19:43:55 +0330

    Add usage log method

--- directadminapi.class.php
+++ directadminapi.class.php
@@ -240,6 +240,20 @@ class DirectAdminAPI {
 
 		return false;
 	}
+
+	/**
+	 * get usage log
+	 * @param  integer $lines number of logs
+	 * @return string
+	 */
+	public function web_usage_log($lines = 0) {
+		$data = $this->request($this->action('CMD_SHOW_LOG') . '?domain=' . $this->domain . '&type=log' . ($lines > 0 ? '&lines=' . $lines : null));
+
+		if ($data !== false)
+			return $data;
+
+		return false;
+	}
 }
 
 ?>
